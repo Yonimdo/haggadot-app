df06062418635ddefaebb7781634428b62c28056
Yoni Mood <yonispark@gmail.com>
2018-02-24 17:54:51 +0200

    inbolet upadate

--- Artifacts/m.py
+++ Artifacts/m.py
@@ -7,21 +7,33 @@ root_directory = 'res'
 
 os.makedirs(STATIC_DIR)
 
+img_template = '''<div class="thumbnail">
+        <img src="{name}" alt="Corsican">
+        <div class="caption">
+            <h4>{name}</h4>
+        </div>
+    </div>'''
+
+html_items = ""
+
 
 def copy_folder_files(root):
+  global html_items
   directory_files = os.listdir(root)
   for name in directory_files:
     path = "{}/{}".format(root, name)
     # If the extension of the file matches some text followed by ext...
     if os.path.isfile(path):
-      shutil.copyfile(path, STATIC_DIR + path.replace("/", "-"))
+      ff = STATIC_DIR + path.replace("/", "-")
+      shutil.copyfile(path, ff)
+      html_items = html_items + (img_template.format(**{'name': ff}))
       # Make a directory with the extension name...
       # os.makedirs(ext)
       pass
     else:
       copy_folder_files(path)
-      # Copy that file to the directory with that extension name
-      #
 
 
 copy_folder_files(root_directory)
+with open('index.html', 'w', encoding='UTF-8') as f:
+  f.write(html_items)
