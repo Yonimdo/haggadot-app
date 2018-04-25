9298811ee2c7b61fec3851dd22c6bea6d1157a1c
Udi Oron <udioron@gmail.com>
2018-04-25 15:06:35 +0300

    Fix deploy URL

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-ng build -d="/static/app/" -bh="/static/app/"
-rsync -av --progress dist/ sysop@nli.oglam.hasadna.org.il:/home/sysop/Hagadot/collected_static/app/
-echo "OK! Now go and see http://nli.oglam.hasadna.org.il/static/app/"
+ng build -d="/app/" -bh="/app/"
+rsync -av --progress dist/ sysop@nli.oglam.hasadna.org.il:/home/sysop/haggadot-app/dist/
+echo "OK! Now go and see http://nli.oglam.hasadna.org.il/app/"
 
