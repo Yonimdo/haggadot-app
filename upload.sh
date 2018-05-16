0c0f6092a9d7ded3460e19d9f5951cc572b80ab0
Udi Oron <udioron@gmail.com>
2018-05-16 11:20:07 +0300

    Update install.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-ng build -d="/app/" -bh="/app/"
+ng build --deploy-url="/app/" --base-href="/app/" --prod
 rsync -av --progress dist/ sysop@nli.oglam.hasadna.org.il:/home/sysop/haggadot-app/dist/
 echo "OK! Now go and see http://nli.oglam.hasadna.org.il/app/"
 
